ff67f85c9d04cfc48a5de64efa4bf48249a9e571
NKA <nkamapper@gmail.com>
2024-03-06 10:42:34 +0100

    Version 1.0
    * Better fix for names
    * Generates updated categories for tagging dict

--- leisure2osm.py
+++ leisure2osm.py
@@ -14,7 +14,7 @@ import urllib.request
 import copy
 
 
-version = "0.4.0"
+version = "1.0.0"
 
 header = {
 	"X-Requested-With": "XMLHttpRequest",
@@ -23,12 +23,69 @@ header = {
 
 
 transform_municipality = {
-	'Tana': 'Deatnu-Tana',
-	'Kautokeino': 'Guovdageaidnu-Kautok',
-	'Karasjok': 'Karasjohka-Karasjok',
-	'Nesseby': 'Unjargga-Nesseby',
-	'Aurskog-Høland': 'Aurskog Høland'
+	'Deatnu-Tana': 'Tana',
+	'Guovdageaidnu-Kautok': 'Kautokeino',
+	'Karasjohka-Karasjok': 'Karasjok',
+	'Unjargga-Nesseby': 'Nesseby',
+	'Aurskog Høland': 'Aurskog-Høland'
 }
+
+transform_name = {
+	'Hus': 'hus',
+	'Grendehus': 'grendehus',
+	'Grendahus': 'grendahus',
+	'Forsamlingshus': 'forsamlingshus',
+	'Samfunnshus': 'samfunnshus',
+	'Ungdomshus': 'ungdomshus',
+	'Aktivitetshus': 'aktivitetshus',
+	'Klubbhus': 'klubbhus',
+	'Velhus': 'velhus',
+	'Bedehus': 'bedehus',
+	'Misjonshus': 'misjonshus',
+	'Menighetshus': 'menighetshus',
+	'Eldrehus': 'eldrehus',
+	'Speiderhus': 'speiderhus',
+	'Kulturhus': 'kulturhus',
+	'Flerbrukshus': 'flerbrukshus',
+	'Kultursenter': 'kultursenter',
+	'Kulturbygg': 'kulturbygg',
+	'Bibliotek': 'bibliotek',
+	'Skole': 'skole',
+	'Skule': 'skule',
+	'Ungdomsskole': 'ungdomsskole',
+	'Ungdomsskule': 'ungdomsskule',
+	'U-skole': 'ungdomsskole',
+	'Videregående': 'videregående'
+}
+
+transform_owner = {
+	'Kommune': 'kommune',
+	'Bygdelag': 'bygdelag',
+	'Idrettsforening': 'idrettsforeing',
+	'Idrettsklubb': 'idrettsklubb',
+	'Idrettslag': 'idrettslag',
+	'Sportsklubb': 'sportsklubb',
+	'Ungdomslag': 'ungdomslag',
+	'I': 'i',
+	'Og': 'og',
+	'For': 'for',
+	'Avd': 'avd',
+	'Al': 'AL',
+	'As': 'AS',
+	'Ba': 'BA',
+	'Da': 'DA',
+	'If': 'IF',
+	'Il': 'IL',
+	'Ik': 'IK',
+	'Sa': 'SA',
+	'Sk': 'SK',
+	'Ul': 'UL',
+	'Kfuk': 'KFUK',
+	'Kfum': 'KFUM',
+	'Kfuk/Kfum': 'KFUK/KFUM',
+	'Kfuk-Kfum': 'KFUK/KFUM'
+}
+
 
 
 # Output message
@@ -55,7 +112,7 @@ def make_osm_line(key,value):
 
 def process_facility (facility):
 
-	global node_id, facilities_ok, facilities_fixed, facilities_noshow, facilities_nocoord, facilities_notfixed
+	global node_id, facilities_ok, facilities_fixed, facilities_noshow, facilities_nocoord, facilities_notfixed, new_category_structure
 
 	if facility['status'] == "EXISTING":
 
@@ -66,8 +123,14 @@ def process_facility (facility):
 		longitude = facility['longitude']
 		message_text = ""
 
-		if facility['municipalityName'] == facility['municipalityName'].upper():
-			facility['municipalityName'] = facility['municipalityName'].title()
+		municipality = facility['municipalityName']
+		if municipality == municipality.upper():
+			municipality = municipality.title()
+		municipality = municipality.replace("Og", "og").replace(" Kommune", "")
+		if municipality in transform_municipality:
+			municipality = transform_municipality[ municipality ]
+
+		ref = facility['countyName'] + "/" + municipality
 
 		if not (latitude and longitude):
 			latitude = 0
@@ -76,16 +139,15 @@ def process_facility (facility):
 			message_text = "No coordinates"
 
 		else:
-			ref = facility['countyName'] + "/" + facility['municipalityName'].replace("Og", "og")
 			bbox = municipalities[ ref ]
 
-			if (latitude < bbox['latitude_min']) or (latitude > bbox['latitude_max']) or \
-				(longitude < bbox['longitude_min']) or (longitude > bbox['longitude_max']):
+			if (latitude < bbox['latitude_min'] or latitude > bbox['latitude_max']
+				or longitude < bbox['longitude_min'] or longitude > bbox['longitude_max']):
 
 				message_text = "Coordinates not within municipality"
 				found = False
 
-				if (latitude >= bbox['latitude_min']) and (latitude <= bbox['latitude_max']):
+				if latitude >= bbox['latitude_min'] and latitude <= bbox['latitude_max']:
 
 					# Attempt relocating longitude to identify correct UTM zone (each 6 degrees appart)
 
@@ -107,7 +169,7 @@ def process_facility (facility):
 
 				# Extra check for Oslo
 
-				if (latitude == 59.917201 and longitude == 10.727413) or (latitude == 59.917112 and longitude == 10.727424):
+				if latitude == 59.917201 and longitude == 10.727413 or latitude == 59.917112 and longitude == 10.727424:
 					message_text = "Not exact coordinates (Oslo)"
 
 		# Produce tags
@@ -121,17 +183,38 @@ def process_facility (facility):
 
 		name = facility['name'].replace(";", ",").strip()
 		if name == name.upper():
-			make_osm_line("name", name.title())
-		else:
-			make_osm_line("name", name)
+			name = name.title()
+
+		name_split = name.split(" ")
+		for i, word in enumerate(name_split):
+			if i > 0 and word in transform_name:
+				name_split[i] = transform_name[ word ]
+		name = " ".join(name_split)
+		make_osm_line("name", name)
 
 		if facility['ownerName']:
-			make_osm_line("owner", facility['ownerName'].strip().title())
+			owner = facility['ownerName'].strip().title()
+			owner_split = owner.split(" ")
+			for i, word in enumerate(owner_split):
+				if word in transform_owner:
+					owner_split[i] = transform_owner[ word ]
+			owner = " ".join(owner_split)
+			make_osm_line("owner", owner)
 
-		if facility['operatorName']:
-			make_osm_line("operator", facility['operatorName'].strip().title())  # None seen in dataset
+		if facility['operatorName'] and facility['operatorName'] != facility['ownerName']:
+			operator = facility['operatorName'].strip().title()
+			operator_split = operator.split(" ")
+			for i, word in enumerate(operator_split):
+				if word in transform_owner:
+					operator_split[i] = transform_owner[ word ]
+			operator = " ".join(operator_split)			
+			make_osm_line("operator", operator)
 
-		make_osm_line("MUNICIPALITY", facility['municipalityName'].replace("Og", "og"))
+
+		if ref in municipalities:
+			make_osm_line("MUNICIPALITY", "#%s %s" % (municipalities[ ref ]['ref'], municipality))
+		else:
+			make_osm_line("MUNICIPALITY", municipality)
 		make_osm_line("COUNTY", facility['countyName'])
 
 		make_osm_line("CATEGORY", facility['categoryDescription'])
@@ -140,11 +223,25 @@ def process_facility (facility):
 		# Generate OSM tagging according to facility type json file
 
 		if facility['typeDescription'] != "UDEFINERT":
+			# Get tags
 			if facility['typeDescription'] in facility_tagging:
 				for key,value in iter(facility_tagging[ facility['typeDescription'] ].items()):
 					make_osm_line(key, value)
 			else:
-				message ("Facility type '%s' in category '%s' not defined\n" % (facility['typeDescription'], facility['categoryDescription']))
+				not_defined.add( (facility['categoryDescription'], facility['typeDescription']) )  # Display at end
+
+			# Build updated category dict
+			if facility['categoryDescription'] not in new_categories:
+				new_categories[ facility['categoryDescription'] ] = {}
+			if facility['typeDescription'] not in new_categories[ facility['categoryDescription'] ]:
+				if facility['typeDescription'] in facility_tagging:
+					tags = facility_tagging[ facility['typeDescription'] ]
+				else:
+					tags = { 'FIXME': 'New facility type' }
+				new_categories[ facility['categoryDescription'] ][ facility['typeDescription'] ] = tags
+
+			if facility['categoryDescription'] not in facility_tagging_data or facility['typeDescription'] not in facility_tagging_data[ facility['categoryDescription'] ]:
+				new_category_structure = True
 
 		out_file.write ('  </node>\n')
 
@@ -186,13 +283,10 @@ if __name__ == '__main__':
 		municipality_data = json.load(file)
 		file.close()
 
-		name = municipality_data['fylkesnavn'] + "/"
-		if municipality_data['kommunenavnNorsk'] in transform_municipality:
-			name += transform_municipality[ municipality_data['kommunenavnNorsk'] ]
-		else:
-			name += municipality_data['kommunenavnNorsk']
+		ref = municipality_data['fylkesnavn'] + "/" + municipality_data['kommunenavnNorsk']
 
 		bbox = {
+			'ref': municipality['kommunenummer'],
 			'latitude_min': 90.0,
 			'latitude_max': -90.0,
 			'longitude_min': 180.0,
@@ -205,7 +299,7 @@ if __name__ == '__main__':
 			bbox['longitude_max'] = max(bbox['longitude_max'], node[0])
 			bbox['longitude_min'] = min(bbox['longitude_min'], node[0])
 
-		municipalities[name] = bbox
+		municipalities[ ref ] = bbox
 
 	# Initiate OSM file and counters
 
@@ -228,11 +322,16 @@ if __name__ == '__main__':
 	last_page = False
 	page = 0
 
+	not_defined = set()  # Will contain new facility categories/types not found in category file
+	new_categories = {}  # Will contain updated facility cateogry dict as read from api
+	new_category_structure = False  # Will become True if category structure has been changed in api
+
 	# Iterate all facilities and produce OSM tags
 
 	while not last_page:
 
-		link = "https://fagsystem.anleggsregisteret.no/idrett/api/facilities?page=%i&size=500&" % page
+#		link = "https://fagsystem.anleggsregisteret.no/idrett/api/facilities?page=%i&size=500&" % page
+		link = "https://backoffice.anleggsregisteret.no/api/facilities?page=%i&size=500" % page
 		request = urllib.request.Request(link, headers=header)
 		file = urllib.request.urlopen(request)
 		facility_data = json.load(file)
@@ -260,3 +359,22 @@ if __name__ == '__main__':
 	message ("  With fixed coordinates       : %i\n" % facilities_fixed)
 	message ("  With incorrect coordinates   : %i\n" % facilities_notfixed)
 	message ("  Wihout coordinates           : %i\n" % facilities_nocoord)
+
+	# Output updated types/categories
+
+	if not_defined:
+		message ("\nFacility types not defined (per category):\n")
+		for missing in sorted(not_defined):
+			message ("  '%s': '%s'\n" % (missing[0], missing[1]))
+
+	if not_defined or new_category_structure:
+		filename = "anleggsregister_kategorier_ny.json"
+		message ("\nSaving new categories to '%s'\n" % filename)
+		file = open(filename, "w")
+		sorted_categories = { key: dict(sorted(new_categories[key].items())) for key in sorted(new_categories) }  # Two level sort
+		json.dump(sorted_categories, file, indent=4, ensure_ascii=False)
+		file.close()
+		if new_category_structure:
+			message ("  Category structure was modified\n")
+
+	message ("\n")
